83d50ca1d1e79fdd1f06d6f39d2f8b9d7913ff33
parwindg <parwinderkumar1008@gmail.com>
2017-04-26 02:19:53 +0530

    Parwinder

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -2,6 +2,15 @@
  * Created by parwindg on 4/24/2017.
  */
 public class LinkedList {
-int i;
-int k;
+    private int length = 0;
+
+    public LinkedList() {
+        length = 0;
+    }
+
+    ListNode head;
+
+    private synchronized ListNode getHead() {
+        return head;
+    }
 }
